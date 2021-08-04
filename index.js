c1daa107559130cb299d8ef5efa8c7b7bd749daf
Triston <tristonherbst97@gmail.com>
2021-08-04 14:03:45 -0500

    schema for new course

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose')
 
+
+
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('connected to mongodb'))
     .catch((err) => console.log('cant connect', err))
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+})
+
+const Course = mongoose.model('Course', courseSchema)
+
+async function createCourse(name, author, tags, isPublished) {
+    const course = new Course({
+        name: name,
+        author: author,
+        tags: tags,
+        isPublished: isPublished
+    })
+    
+    const result = await course.save()
+    
+    console.log(result)
+    
+}
+
+createCourse('NodeJS', 'Mosh', ['node','backend'], true)
