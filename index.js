848846f03393d68b59f110271e3eb68d0c5fbf96
Triston <tristonherbst97@gmail.com>
2021-08-05 12:17:44 -0500

    using comparison queries

--- index.js
+++ index.js
@@ -30,15 +30,28 @@ async function createCourse(name, author, tags, isPublished, date) {
 }
 
 async function getCourses() {
+    // $eq (equal to)
+    // $ne (not equal to)
+    // $gt (greater than)
+    // $gte (greater than or equal to)
+    // $lt
+    // $lte
+    // $in (included in an array)
+    // $nin (not included in an array)
+    // .find({price: { $gte: 10 } }) finding objects with a price of 10 or higher
+    // .find({price: { $in: [5,10,20] } }) finding objects with prices of 5 10 and 20
+
+
     const courses = await Course
         .find({author: 'Mosh'})
-        .limit(1)
+        .limit(3)
         .sort({name: 1})
+        .select({name: 1, tags: 1})
     console.log(courses)
 }
 
-createCourse('React', 'Mosh', ['react','frontend'], true)
-createCourse('Node', 'Mosh', ['node','backend'], true)
-createCourse('express', 'Mosh', ['node','backend'], true)
+// createCourse('React', 'Mosh', ['react','frontend'], true)
+// createCourse('Node', 'Mosh', ['node','backend'], true)
+// createCourse('express', 'Mosh', ['node','backend'], true)
 
 getCourses()
