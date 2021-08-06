58f9fa517a222cc3122963735ca764d5589fd7d1
Triston <tristonherbst97@gmail.com>
2021-08-06 14:43:33 -0500

    exercises complete

--- index.js
+++ index.js
@@ -17,23 +17,32 @@ const courseSchema = new mongoose.Schema({
 
 const Course = mongoose.model("Course", courseSchema);
 
+async function updateCourse(id){
+  
+}
+
 async function getCourses1() {
-  const courses = await Course
-    .find({isPublished: true, tags: 'backend'})
+  const courses = await Course.find({ isPublished: true, tags: "backend" })
     .sort({ name: 1 })
     .select({ name: 1, author: 1 });
 
   console.log(courses);
 }
 
-
-async function getCourses2(){
-    const courses = await Course
-        .find({isPublished: true })
-        .or([{tags: 'frontend'}, {tags: 'backend'}])
-        .sort({price: -1})
-        .select('name author price')
-    console.log(courses)
+async function getCourses2() {
+  const courses = await Course.find({ isPublished: true })
+    .or([{ tags: "frontend" }, { tags: "backend" }])
+    .sort({ price: -1 })
+    .select("name author price");
+  console.log(courses);
 }
 
-getCourses2();
+async function getCourses3() {
+  const courses = await Course.find().or([
+    { price: { $gte: 15 } },
+    { name: /.*by.*/ },
+  ]);
+  console.log(courses)
+}
+
+// getCourses3();
